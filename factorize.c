e3cbf920ff6ee33dd44d7502cd708f7c2111b645
AZIZEM81 <eaziz428@gmail.com>
2024-04-01 09:43:54 -0700

    encryption

--- factorize.c
+++ factorize.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to factorize a number
 void factorize(int num) {
-	int divisor = 2;
-	while (num > 1) {
-		while (num % divisor == 0) {
-			printf("%d*", divisor);
-			num /= divisor;
-		}
-		divisor++;
-	}
+	int factor1 = 2;
+	int factor2 = num / factor1;
+	printf("%d=%d*%d\n", num, factor1, factor2);
 }
 
 int main(int argc, char *argv[]) {
-	// Check if a filename is provided as an argument
 	if (argc != 2) {
 		printf("Usage: %s <filename>\n", argv[0]);
 		return (1);
 	}
 
-	// Open the file
 	FILE *file = fopen(argv[1], "r");
 	if (file == NULL) {
 		perror("Error opening file");
 		return (1);
 	}
 
-	// Read each line from the file and factorize the numbers
 	char buffer[100];
 	while (fgets(buffer, sizeof(buffer), file) != NULL) {
 		int num = atoi(buffer);
-		printf("%d=", num);
+		if (num <= 1) {
+			printf("Invalid number: %d\n", num);
+			continue;
+		}
 		factorize(num);
-		printf("\n");
 	}
 
-	// Close the file
 	fclose(file);
-	return(0);
+	return (0);
 }
